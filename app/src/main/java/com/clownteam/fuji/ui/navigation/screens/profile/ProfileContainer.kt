5538f8b80158a51ba8a71a4eee15acaac91907aa
kmem1 <konstantinovich.aleks@mail.ru>
2022-05-28 11:57:04 +0300

    Refactor ProfileContainer

--- app/src/main/java/com/clownteam/fuji/ui/navigation/screens/profile/ProfileContainer.kt
+++ app/src/main/java/com/clownteam/fuji/ui/navigation/screens/profile/ProfileContainer.kt
@@ -2,6 +2,7 @@ package com.clownteam.fuji.ui.navigation.screens.profile
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import com.clownteam.fuji.ui.navigation.NavigationController
 import com.clownteam.fuji.ui.navigation.Route
@@ -20,40 +21,52 @@ fun ProfileContainer(externalRouter: Router) {
         router = externalRouter,
         screens = listOf(
             Pair(Route.LoginRoute.route) { navController, _, _ ->
-                val viewModel: LoginViewModel = hiltViewModel()
-                LoginScreen(
-                    state = viewModel.state,
-                    eventHandler = viewModel,
-                    viewModel = viewModel,
-                    navigateToRegistration = { navController.navigate(Route.RegistrationRoute.route) },
-                    navigateToRestorePassword = { navController.navigate(Route.RestorePasswordRoute.route) })
+                openLoginScreen(navController)
             },
 
             Pair(Route.RegistrationRoute.route) { navController, _, _ ->
-                val viewModel: RegistrationViewModel = hiltViewModel()
-                RegistrationScreen(
-                    state = viewModel.state,
-                    eventHandler = viewModel,
-                    viewModel = viewModel,
-                    navigateToLogin = {
-                        navController.navigate(
-                            Route.LoginRoute.route,
-                            navOptions = NavOptions.Builder()
-                                .setPopUpTo(Route.LoginRoute.route, true).build()
-                        )
-                    },
-                    onSuccessRegistration = {}
-                )
+                openRegistrationScreen(navController)
             },
 
             Pair(Route.RestorePasswordRoute.route) { navController, _, _ ->
-                val viewModel: RestorePasswordViewModel = hiltViewModel()
-                RestorePasswordScreen(
-                    state = viewModel.state,
-                    eventHandler = viewModel,
-                    navigateBack = { navController.popBackStack() }
-                )
+                openRestorePasswordScreen(navController)
             }
         )
+    )
+}
+
+private fun openLoginScreen(navController: NavController) {
+    val viewModel: LoginViewModel = hiltViewModel()
+    LoginScreen(
+        state = viewModel.state,
+        eventHandler = viewModel,
+        viewModel = viewModel,
+        navigateToRegistration = { navController.navigate(Route.RegistrationRoute.route) },
+        navigateToRestorePassword = { navController.navigate(Route.RestorePasswordRoute.route) })
+}
+
+private fun openRegistrationScreen(navController: NavController) {
+    val viewModel: RegistrationViewModel = hiltViewModel()
+    RegistrationScreen(
+        state = viewModel.state,
+        eventHandler = viewModel,
+        viewModel = viewModel,
+        navigateToLogin = {
+            navController.navigate(
+                Route.LoginRoute.route,
+                navOptions = NavOptions.Builder()
+                    .setPopUpTo(Route.LoginRoute.route, true).build()
+            )
+        },
+        onSuccessRegistration = {}
+    )
+}
+
+private fun openRestorePasswordScreen(navController: NavController) {
+    val viewModel: RestorePasswordViewModel = hiltViewModel()
+    RestorePasswordScreen(
+        state = viewModel.state,
+        eventHandler = viewModel,
+        navigateBack = { navController.popBackStack() }
     )
 }
